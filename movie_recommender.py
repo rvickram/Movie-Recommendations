2c9f7bcdd2493a4561a6c1e5716803f4477cf79c
Ryan Vickramasinghe <rvickram@uwo.ca>
2020-10-27 18:50:55 -0400

    Calculate count matrix from features.

--- movie_recommender.py
+++ movie_recommender.py
@@ -24,6 +24,10 @@ for feature in features:
 ## Combine the features into a single string
 def combine_features(row):
 	return row['keywords']+" "+row['cast']+" "+row['genres']+" "+row['director']
-
+# create new column with combined features
 df['combined_features'] = df.apply(combine_features, axis=1) # axis=1 means pass rows not columns
 print(df['combined_features'].head())
+
+## Create count matrix from this new combined column
+cv = CountVectorizer()
+count_matrix = cv.fit_transform(df['combined_features'])
