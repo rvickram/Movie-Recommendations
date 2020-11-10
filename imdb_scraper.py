1e54d301e296a9f26b9ba13fa245ae4f8e53671e
Ryan John Vickramasinghe <rvickram@uwo.ca>
2020-11-10 15:54:30 -0500

    Created separate file for parser functions.

--- imdb_scraper.py
+++ imdb_scraper.py
@@ -2,7 +2,9 @@
 # Scrapes IMDB to get latest TV shows and data
 
 import requests
+import csv
 from bs4 import BeautifulSoup
+from parsers import *
 
 # This function gets a BeautifulSoup object from a given url
 def getPage(url):
@@ -25,56 +27,42 @@ def getMovieUrlList(soupPage):
         urls.append('https://www.imdb.com' + titleLink)
     return urls
 
+# This function will retrieve all the movie urls on the given search page, and add them to the data frame
+def writeData(searchPageSoup, writer):
+    # get urls for first movie
+    urlList = getMovieUrlList(searchPageSoup)
+
+    # get dictionary for each movie
+    for url in urlList:
+        movieData = scrapeMoviePage(url)
+        for data in movieData:
+            writer.writerow(data)
+
 # This will scrape an IMDB tv show page for information and return a dictionary
 def scrapeMoviePage(url):
     # fetch the live page
     page = getPage(url)
 
     # scrape the title
-    title = page.select_one('.title_wrapper') \
-        .select_one('h1').getText()
-    title = title.split('\xa0', 2)[0].strip()
+    title = scrapeTitle(page)
 
     # get keywords
-    keywords = page.select('.itemprop')
-    keywordsParsed = []
-    for keyword in keywords:
-        keywordsParsed.append(keyword.getText().strip())
+    keywordsParsed = scrapeKeywords(page)
 
     # get genre
-    genre = page.select('.see-more.inline.canwrap')[1].select_one('a').getText().strip()
+    genre = scrapeGenre(page)
 
-    # get creators
-    creators = page.select('.credit_summary_item')[0].select('a')
-    creatorsParsed = []
-    for creator in creators:
-        creatorsParsed.append(creator.getText().replace(' ', ',').strip())
-
-    # get stars
-    stars = page.select('.credit_summary_item')[1].select('a')
-    starsParsed = []
-    for star in stars:
-        starsParsed.append(star.getText().replace(' ', ',').strip())
-    del starsParsed[len(starsParsed) - 1] # remove the 'see full cast link'
+    # check for both creators and stars
+    creatorsParsed, starsParsed = scrapeCreatorsStars(page)
 
     # get rating
-    rating = page.find('span', itemprop='ratingValue').getText().strip()
+    rating = scrapeRating(page)
 
     # get details
-    detailsDiv = page.select_one('.article#titleDetails').select('div')
-    country = detailsDiv[1].select_one('a').getText().strip()
-    language = detailsDiv[2].select_one('a').getText().strip()
-    releaseYear = detailsDiv[3].getText().split()[4]
-    productionCos = detailsDiv[6].select('a')
-    # parse production companies
-    productionCosParsed = []
-    for company in productionCos:
-        companyText = company.getText().strip()
-        if companyText != 'See more':
-            productionCosParsed.append(companyText.replace(' ', ','))
+    country, language, releaseYear, productionCosParsed = scrapeDetails(page)
 
     ## build the dictonary to return
-    data = {
+    data = [{
         'title' : title,
         'keywords' : ' '.join(keywordsParsed),
         'genre' : genre,
@@ -85,19 +73,46 @@ def scrapeMoviePage(url):
         'language' : language,
         'release' : releaseYear,
         'production_company' : ' '.join(productionCosParsed)
-    }
+    }]
     return data
 
-# get the first page
-soup = getPage('https://www.imdb.com/search/title/?title_type=tv_series,tv_special,tv_miniseries,documentary,tv_short')
-# get the toal number of results (so we know how many pages to crawl)
-numResults = getNumResults(soup)
-print('Total results: ', numResults)
 
-# get urls for first movie
-urlList = getMovieUrlList(soup)
+## DRIVER CODE
 
-# get dictionary for each movie
-for url in urlList:
-    movieData = scrapeMoviePage(url)
-    print(movieData)
+# headers
+csv_cols = ['title', 'keywords', 'genre', 'creators', 'stars', 'rating',\
+    'country_of_origin', 'language', 'release', 'production_company']
+
+csv_file = "scraped_data.csv"
+try:
+    print('Opening file for writing...')
+    with open(csv_file, 'w', newline='') as csvfile:
+        writer = csv.DictWriter(csvfile, fieldnames=csv_cols)
+        writer.writeheader()
+        print('Wrote header to file.')
+
+        # get the first page
+        searchUrl = 'https://www.imdb.com/search/title/?title_type=tv_series,tv_special,tv_miniseries,documentary,tv_short'
+        soup = getPage(searchUrl)
+        # get the toal number of results (so we know how many pages to crawl)
+        numResults = getNumResults(soup)
+        print('Total results: ', numResults)
+
+        print('Processing page 1')
+        writeData(soup, writer)
+
+        currentSearchPage = 51
+        numRuns = int(numResults) // 50
+        if int(numResults) % 50 == 0:
+            numRuns -= 1
+        
+        for i in range(numRuns):
+            print('Processing page ' + str(i + 2))
+            url = searchUrl + '&start=' + str(currentSearchPage)
+            currentSearchPage += 50
+
+            soup = getPage(url)
+            writeData(soup, writer)
+
+except IOError:
+    print("I/O error")
---
+++ parsers.py
@@ -0,0 +1,82 @@
+# Created by: Ryan Vickramasinghe
+# This class holds parsing functions for processing scraped data
+
+from bs4 import BeautifulSoup
+
+def scrapeTitle(page):
+    title = page.select_one('.title_wrapper') \
+        .select_one('h1').getText()
+    title = title.split('\xa0', 2)[0].strip()
+
+    return title
+
+def scrapeKeywords(page):
+    keywords = page.select('.itemprop')
+    keywordsParsed = []
+    for keyword in keywords:
+        keywordsParsed.append(keyword.getText().strip())
+
+    return keywordsParsed
+
+def scrapeGenre(page):
+    return page.select('.see-more.inline.canwrap')[1].select_one('a').getText().strip()
+
+def scrapeCreatorsStars(page):
+    creatorStars = page.select('.credit_summary_item')
+    creatorsParsed = []
+    starsParsed = []
+    for item in creatorStars:
+        header = item.select_one('h4').getText().strip()
+        if(header == 'Creators:'):
+            # get creators
+            creators = item.select('a')
+            for creator in creators:
+                creatorsParsed.append(creator.getText().replace(' ', ',').strip())
+        elif (header == 'Stars:'):
+            # get stars
+            stars = item.select('a')
+            for star in stars:
+                starsParsed.append(star.getText().replace(' ', ',').strip())
+            del starsParsed[len(starsParsed) - 1] # remove the 'see full cast link'
+    
+    return creatorsParsed, starsParsed
+
+def scrapeRating(page):
+    ratingItem = page.find('span', itemprop='ratingValue')
+    rating = ''
+    if ratingItem:
+        rating = ratingItem.getText().strip()
+    
+    return rating
+
+def scrapeDetails(page):
+        detailsDiv = page.select_one('.article#titleDetails').select('div')
+
+        country = ''
+        language = ''
+        releaseYear = ''
+        productionCosParsed = []
+
+        for div in detailsDiv:
+            header = div.select_one('h4')
+            if (header == None): 
+                continue
+            else:
+                header = header.getText().strip()
+
+            if (header == 'Country:'):
+                country = div.select_one('a').getText().strip()
+            elif (header == 'Language:'):
+                language = div.select_one('a').getText().strip()
+            elif (header == 'Release Date:'):
+                releaseYear = div.getText().split()[4]
+            elif (header == 'Production Co:'):
+                productionCos = div.select('a')
+                # parse production companies
+                productionCosParsed = []
+                for company in productionCos:
+                    companyText = company.getText().strip()
+                    if companyText != 'See more':
+                        productionCosParsed.append(companyText.replace(' ', ','))
+
+        return country, language, releaseYear, productionCosParsed
